856b0cb1b03db62669365645b19970bc96a02409
StasAstenenko <107870729+StasAstenenko@users.noreply.github.com>
2024-12-21 16:34:36 +0200

    .

--- src/redux/details/operations.js
+++ src/redux/details/operations.js
@@ -2,11 +2,10 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { instance } from '../api/api';
 
 export const getTsById = createAsyncThunk(
-  'camper/idCamper',
+  '/campers/:id',
   async (id, thunkApi) => {
     try {
       const { data } = await instance.get(`/campers/${id}`);
-      console.log(data);
       return data;
     } catch (error) {
       return thunkApi.rejectWithValue(error.message);
